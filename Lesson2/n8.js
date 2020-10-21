fd4d5e99d62c3522e7e377e38f742538bed63cbf
zinaidamh <72624597+zinaidamh@users.noreply.github.com>
2020-10-21 22:00:43 +0300

    Update n8.js
    added fullname setter

--- Lesson2/n8.js
+++ Lesson2/n8.js
@@ -2,19 +2,17 @@
 //1. При записи значения в свойство fullName имя и фамилия должны записываться в отдельные свойства firstName и lastName
 //2. При чтении значения fullName оно должно получаться из объединения firstName и lastName
 //3. При задании значения свойства dateOfBirth должно так же устанавливаться свойство age в зависимости от разницы года рождения и текущего года
-
-
 function getAge(dateString) 
 {
-    var today = new Date();
-    var birthDate = new Date(dateString);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
-    {
-        age--;
-    }
-    return age;
+   let today = new Date();
+   let birthDate = new Date(dateString);
+   let age = today.getFullYear() - birthDate.getFullYear();
+   let m = today.getMonth() - birthDate.getMonth();
+   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+   {
+       age--;
+   }
+   return age;
 }
 
 const human = 
@@ -25,14 +23,23 @@ const human =
    {
       return `${this.firstName} ${this.lastName} `  
    },
+   set fullname(value)
+   {
+      let arr = value.split(" ")
+      this.firstName=arr[0]
+      this.lastName=arr[1]
+   },
    set dateOfBirth(birthDateString)
    {
        this.birthDate=new Date(birthDateString)
        this.age=getAge(birthDateString)
    }
- }
+}
 
 console.log(human.fullname)
 human.dateOfBirth= '05/07/1990'
+human.fullname="John Brown"
 console.log(human.birthDate)
 console.log(human.age)
+console.log(human.firstName, human.lastName)
+
